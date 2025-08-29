74ef909933bbc33f8c452ae7fd31ea1a2c490b44
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2025-08-29 13:56:24 -0400

    Added methods to the shape of our API. Little weird architecture choice but whatever

--- core/src/api.rs
+++ core/src/api.rs
@@ -1,5 +1,10 @@
 use async_trait::async_trait;
-use crate::{Result};
+use reqwest::header::HeaderMap;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+use std::fmt::{Debug};
+use crate::http_api::SupportedHttpMethods;
+use anyhow::{Result};
 use crate::dtos::responses::telemetry_snapshot::TelemetrySnapshot;
 
 #[async_trait]
@@ -9,4 +14,48 @@ pub trait DroneApi: Send + Sync {
     async fn takeoff(&mut self, altitude_m: f32) -> Result<()>;
     async fn land(&mut self) -> Result<()>;
     async fn status(&mut self) -> Result<TelemetrySnapshot>;
+    async fn get<T>(&mut self, endpoint: &String) -> Result<Option<T>> where T: DeserializeOwned + Serialize + Debug;
+    async fn post<T, B>(&mut self, endpoint: &String, body: Option<&B>) -> anyhow::Result<T> where T: DeserializeOwned + Serialize + Debug + Clone, B: Serialize + ?Sized + Sync;
+    async fn get_with_headers<T>(&mut self, endpoint: &str, extra: Option<HeaderMap>) -> Result<Option<T>> where T: DeserializeOwned + Serialize + Debug;
+    async fn post_with_headers<T, B>(
+        &mut self,
+        endpoint: &str,
+        body: Option<&B>,
+        extra: Option<HeaderMap>
+    ) -> anyhow::Result<T>
+    where
+        T: DeserializeOwned + Serialize + Debug + Clone,
+        B: Serialize + ?Sized + Sync;
+    
+    async fn _send_request<T, B>(
+        &mut self,
+        endpoint: &str,
+        http_method: SupportedHttpMethods,
+        body: Option<&B>,
+        extra_headers: Option<HeaderMap>,
+    ) -> Result<Option<T>>
+    where
+        T: DeserializeOwned,
+        B: Serialize + ?Sized + Sync;
+    
+    async fn _get_request_by_http<T>(
+        &mut self,
+        endpoint: &str,
+        extra: Option<HeaderMap>,
+    ) -> Result<Option<T>>
+    where
+        T: DeserializeOwned;
+
+    async fn _post_request_by_http<T, B>(
+        &mut self,
+        endpoint: &str,
+        body: Option<&B>,
+        extra: Option<HeaderMap>,
+    ) -> Result<T>
+    where
+        T: DeserializeOwned,
+        B: Serialize + ?Sized + Sync;
+
+    fn retrieve_headers(&mut self, token: &String) -> anyhow::Result<HeaderMap>;
+    fn get_my_token(&mut self) -> String;
 }
